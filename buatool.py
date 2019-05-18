5b7fea441d6aa967302b128e5c5a1037c09f8b8f
Tyler Ward <tyler@scorpia.co.uk>
2019-05-18 22:29:49 +0100

    Added SHA1 calculation and ability to load and save the index

--- buatool.py
+++ buatool.py
@@ -1,10 +1,10 @@
-#!/bin/python3
+#!/usr/bin/python3
 
 import os
 import sys
 import filecmp
 import hashlib
-
+import click
 
 
 def calculateSHA1Sum(filename):
@@ -18,20 +18,24 @@ def calculateSHA1Sum(filename):
     return sha1.hexdigest()
 
 
-def generateLibraryRecords(directory):
+def generateLibraryRecords(directory,sha1=False):
     """Create a library record for a target directory"""
 
     fileindex=[]
 
     for (dirpath, dirnames, filenames) in os.walk(directory):
         for filename in filenames:
-            filedetails={
-                    'name':filename,
-                    'folder':dirpath,
-                    'fullpath':dirpath+"/"+filename,
-                    'sha1':calculateSHA1Sum(dirpath+"/"+filename)
-                    }
-            fileindex.append(filedetails)
+            try:
+                filedetails={
+                        'name':filename,
+                        'folder':dirpath,
+                        'fullpath':dirpath+"/"+filename,
+                        }
+                if sha1:
+                    filedetails['sha1']=calculateSHA1Sum(dirpath+"/"+filename)
+                fileindex.append(filedetails)
+            except (FileNotFoundError,PermissionError):
+                print("Unable to index "+dirpath+"/"+filename)
     return fileindex
 
 
@@ -41,43 +45,69 @@ def findFile(name,index):
 def findHash(sha1,index):
     return(list(filter(lambda filed: filed['sha1'] == sha1,index)))
 
-def findMatches(filename,filedir,index):
+def findMatches(filename,filedir,index,sha1=False):
 
     matches = []
     filesha1 = calculateSHA1Sum(filedir+"/"+filename)
 
     namematches = findFile(filename,index)
     for match in namematches:
-        if filesha1 == match['sha1']:
+        if sha1 and filesha1 == match['sha1']:
             if filecmp.cmp(filedir+"/"+filename,match['fullpath']):
                 matches.append(match)
     if len(matches)>0:
         return matches
-
-    hashmatches = findHash(filesha1,index)
-    for match in hashmatches:
-        if filecmp.cmp(filedir+"/"+filename,match['fullpath']):
-            matches.append(match)
+    if sha1:
+        hashmatches = findHash(filesha1,index)
+        for match in hashmatches:
+            if filecmp.cmp(filedir+"/"+filename,match['fullpath']):
+                matches.append(match)
 
     return matches
 
 
 
 
-def evaluateDirectory(directory,index):
+def evaluateDirectory(directory,index,sha1=False):
     """Evaluates a directory an produces a result list"""
 
     for (dirpath, dirnames, filenames) in os.walk(directory):
         for filename in filenames:
-            matches = findMatches(filename,dirpath,index)
+            matches = findMatches(filename,dirpath,index,sha1=sha1)
             if len(matches)==0:
                 print("Missing:"+dirpath+"/"+filename)
             else:
                 if len(list(filter(lambda filed: filed['name'] == filename,matches)))==0:
                     print("Renamed:"+dirpath+"/"+filename+"-->"+matches[0]['fullpath'])
 
+def saveIndex(index,location):
+    import json
+    with open(location,"w") as output:
+        json.dump(index,output)
 
-index = generateLibraryRecords(sys.argv[2])
-evaluateDirectory(sys.argv[1],index)
+def loadIndex(location):
+    import json
+    with open(location,"r") as indexInput:
+        return(json.load(indexInput))
 
-exit()
+
+@click.command()
+@click.option('--sha1',default=False,is_flag=True,help="Compare files using their sha1sum as well as the name")
+@click.option('--save-index',default=None,help="Location to save index to for later use")
+@click.option('--load-index',default=None,help="Location to save index to for later use")
+@click.argument('target')
+@click.argument('reference')
+def buatool(target,reference,sha1,load_index,save_index):
+
+    if load_index != None:
+        index = loadIndex(load_index)
+    else:
+        index = generateLibraryRecords(reference,sha1=sha1)
+    if save_index != None:
+        saveIndex(index,save_index)
+    evaluateDirectory(target,index,sha1=sha1)
+
+
+if __name__ == "__main__":
+    buatool()
+    exit()
